e1ceb0b4f3b7ff364236ee2c3e6aa3545d7e051a
Iker <irodrigueziglesias@danielcastelao.org>
2022-10-10 09:51:08 +0200

    Añadidas imágenes con temática pokemon + botón e idioma "Alemán"

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -16,8 +16,10 @@
 
 package com.example.android.diceroller
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
@@ -26,11 +28,12 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
+        val rollButton: ImageButton= findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
         }
@@ -41,12 +44,12 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
         val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+            1 -> R.drawable.charmander
+            2 -> R.drawable.bulba
+            3 -> R.drawable.mudkip
+            4 -> R.drawable.squirtle
+            5 -> R.drawable.torchic
+            else -> R.drawable.treecko
         }
 
         diceImage.setImageResource(drawableResource)
